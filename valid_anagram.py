a4380c1ad86893b5259bd7dc1a83ed76b9623c69
Hisham <hisham.khashman@gmail.com>
2026-06-05 01:39:40 +0900

    validate anagrams correct solution using sorted

--- valid_anagram.py
+++ valid_anagram.py
@@ -2,14 +2,14 @@
 
 # assume two strings s1 and s2
 
-s1 = "evil"
-s2 = "vile"
+s1 = "hello"
+s2 = "world"
 
 # TODO : validate if two strings are anagrams (meaning have the same letters with the same frequencies)
 
 def validate_anagrams(s1,s2):
     #TODO: validate that they first have the same letters
-    if list(s1).sort() == list(s2).sort():
+    if sorted(s1) == sorted(s2):
         return True
     return False
 print (validate_anagrams(s1,s2))
