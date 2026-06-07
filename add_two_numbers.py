c5fdce2a93172da3b4b0bfadccc11e3f53f81499
Hisham <hisham.khashman@gmail.com>
2026-06-07 15:17:52 +0900

    added string pattern matching question

--- add_two_numbers.py
+++ add_two_numbers.py
@@ -7,21 +7,21 @@
 # Output: [7,0,8]
 # Explanation: 342 + 465 = 807.
 
-l1 = [2,4,3]
-l2 = [5,6,4]
+l1 = [9,9,9,9,9,9,9]
+l2 = [9,9,9,9]
 
-l1_rev = l1[::-1]
-l2_rev = l2[::-1]
+#l1_rev = l1[::-1]
+#l2_rev = l2[::-1]
 
-print ("".join(map(str,l1_rev)))
+#print ("".join(map(str,l1_rev)))
 #use int(str) to convert to int
 
 def add_two_nums(l1,l2):
     l1_int_reverse = int("".join(map(str,l1[::-1])))
     l2_int_reverse = int("".join(map(str,l2[::-1])))
-    sum = l1_int_reverse + l2_int_reverse
+    total = l1_int_reverse + l2_int_reverse
 
-    reversed_sum = int(str(sum)[::-1])
+    reversed_sum = int(str(total)[::-1])
 
     result = list(map(int, str(reversed_sum)))
     return result
---
+++ string_pattern_matching.py
@@ -0,0 +1,5 @@
+# You are given two strings, pattern and source.
+
+# The string pattern contains only the characters '0' and '1'.
+
+# The string source contains only lowercase English letters.
