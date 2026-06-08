23a65c80de7a94a3bcb92eeaf265c0606e9cea46
Hisham <hisham.khashman@gmail.com>
2026-06-09 00:40:24 +0900

    matrix border sum done

---
+++ matrix_border_sum.py
@@ -0,0 +1,23 @@
+matrix = [
+        [1,2,3],
+        [4,5,6],
+        [7,8,9]
+]
+
+
+# i is for matrix range
+# j is for column range
+def solution_border_sum(matrix):
+    border_nums_rows = []
+    border_nums_columns = []
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if i == 0 or i == len(matrix)-1:
+                border_nums_rows.append(matrix[i][j])
+            if j == 0 or j == len(matrix[i])-1:
+                border_nums_columns.append(matrix[i][j])
+        dup_nums_sum = matrix[0][0] + matrix[0][len(matrix[i])-1] + matrix[len(matrix)-1][0] + matrix[len(matrix)-1][len(matrix[i])-1]
+        sum_borders = sum(border_nums_rows) + sum(border_nums_columns) - dup_nums_sum
+    return sum_borders
+print(solution_border_sum(matrix))
+
--- secondary_diagonal_sum.py
+++ secondary_diagonal_sum.py
@@ -9,13 +9,10 @@ def solution_secondary_diag_sum(matrix):
     diag_num = []
     for row in matrix:
         rev_matrix.append(row[::-1])
-        print (rev_matrix)
 
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
+    for i in range(len(rev_matrix)):
+        for j in range(len(rev_matrix[i])):
                        if i == j:
-                           diag_num.append(matrix[i][j])
+                           diag_num.append(rev_matrix[i][j])
     return sum(diag_num)
-
-print (solution_secondary_diag_sum(matrix))
 
