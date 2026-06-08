17c54330d681bced25c4c8508e952776f9652feb
Hisham <hisham.khashman@gmail.com>
2026-06-08 15:45:07 +0900

    looping through items

--- string_pattern_matching.py
+++ string_pattern_matching.py
@@ -14,4 +14,27 @@
 
 # eample for pattern = "010" and source = "amazing" , out put would be 2. ama = 010  and azi = 010
 
+pattern = "010"
 
+source = "amazing"
+
+vowels = ["a", "i", "e", "o", "u", "y"]
+
+arr_source = list(source)
+
+print (arr_source)
+
+for item in arr_source:
+    if item in vowels:
+        print ("0")
+    else:
+        print ("1")
+# convert string source to array
+
+
+
+#loop through items in array and check them against vowels, if match change them to 0 , if anything else 1
+
+# slice 3 items and loop through 3 items to match pattern, if match count += 1
+
+# return count
