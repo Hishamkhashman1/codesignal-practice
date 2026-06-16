b112b671b4aaacafe248ac111508ad560b707434
Hisham <hisham.khashman@gmail.com>
2026-06-17 00:42:12 +0900

    added increasing triple sequence O(n^2)

---
+++ leetcode_238_product_of_array_except_self.py
@@ -0,0 +1,52 @@
+# 238. Product of Array Except Self
+# Medium
+# Topics
+# premium lock icon
+# Companies
+# Hint
+# Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+#
+# The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+#
+# You must write an algorithm that runs in O(n) time and without using the division operation.
+#
+#
+#
+# Example 1:
+#
+# Input: nums = [1,2,3,4]
+# Output: [24,12,8,6]
+# Example 2:
+#
+# Input: nums = [-1,1,0,-3,3]
+# Output: [0,0,9,0,0]
+#
+#
+# Constraints:
+#
+# 2 <= nums.length <= 105
+# -30 <= nums[i] <= 30
+# The input is generated such that answer[i] is guaranteed to fit in a 32-bit integer.
+import math
+nums = [1,2,3,4]
+
+def solution(nums):
+    products = []
+
+    for i in range (len(nums)):
+        # if i == 0:
+        #     product = math.prod(nums[i+1 : len(nums)])
+        #     products.append(product)
+        # elif i == -1:
+        #     product = math.prod(nums[0 : -2])
+        #     products.append(product)
+        #
+        # else:
+        product = math.prod(nums[:i] + nums[i+1 :len(nums)])
+        products.append(product)
+
+
+    return products
+
+print (solution(nums))
+
---
+++ leetcode_334_increasing_triplet_sequence.py
@@ -0,0 +1,67 @@
+# 334. Increasing Triplet Subsequence
+# Medium
+# Topics
+# premium lock icon
+# Companies
+# Given an integer array nums, return true if there exists a triple of indices (i, j, k) such that i < j < k and nums[i] < nums[j] < nums[k]. If no such indices exists, return false.
+#
+#
+#
+# Example 1:
+#
+# Input: nums = [1,2,3,4,5]
+# Output: true
+# Explanation: Any triplet where i < j < k is valid.
+# Example 2:
+#
+# Input: nums = [5,4,3,2,1]
+# Output: false
+# Explanation: No triplet exists.
+# Example 3:
+#
+# Input: nums = [2,1,5,0,4,6]
+# Output: true
+# Explanation: One of the valid triplet is (1, 4, 5), because nums[1] == 1 < nums[4] == 4 < nums[5] == 6.
+#
+#
+# Constraints:
+#
+# 1 <= nums.length <= 5 * 105
+# -231 <= nums[i] <= 231 - 1
+nums =[20,100,10,12,5,13]
+
+# def solution(nums):
+#     check = 0   
+#     for i in range (len(nums)-2):
+#             for j in range (i +1, len(nums)-1):
+#                 for k in range (j +1, len(nums)):
+#                     if nums[i] < nums[j] < nums[k]:
+#                          check +=1
+#                          print (nums[i],nums[j], nums[k])
+#
+#     if check > 0:
+#         return True
+#     else:
+#         return False
+#
+# print (solution(nums))
+
+def solution_on2(nums):
+    for j in range(1, len(nums) -1):
+        left_smaller = False
+        right_bigger = False
+
+        for i in range(0, j):
+            if nums[i] < nums [j] :
+                left_smaller = True
+
+        for k in range (j +1, len(nums)):
+            if nums [j] < nums [k] :
+                right_bigger = True
+
+        if left_smaller and right_bigger:
+            return True
+
+    return False
+
+print (solution_on2(nums))
